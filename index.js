45cc0d2c38998d8864580295bd0e89fadf8db396
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2020-10-13 17:54:49 +0200

    Add elements queries

---
+++ index.js
@@ -0,0 +1,10 @@
+const zoom = document.querySelector('#zoom');
+const weeks = document.querySelector("#weeks");
+const tracks = document.querySelector("#tracks");
+const certificate = document.querySelector("#certificate");
+const guide = document.querySelector("#guide");
+const canals = document.querySelector('#canals');
+const tools = document.querySelector('#tools');
+const communities = document.querySelector('#communities');
+const statusPages = document.querySelector('#statusPages');
+
