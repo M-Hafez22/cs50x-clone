3b77717f454f3fa7e59e7206395c37b4c1e28e96
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2020-10-13 18:01:04 +0200

    Add weeks list

--- index.js
+++ index.js
@@ -8,3 +8,32 @@ const tools = document.querySelector('#tools');
 const communities = document.querySelector('#communities');
 const statusPages = document.querySelector('#statusPages');
 
+// Generte weeks items
+
+function generateweeksList(arr){
+    for (let i = 0; i < arr.length; i++){
+                
+        // Create a list item
+        let week = document.createElement("li");
+        
+        // Create a list item Link
+        let weekLink = document.createElement("a");
+        weekLink.textContent = `Week ${i}`
+        weekLink.setAttribute('href', `https://cs50.harvard.edu/x/2020/weeks/${i}`)
+ 
+        // Create a list item Name
+        let weekName = document.createElement("span");
+        weekName.textContent =" " + arr[i];
+        weekName.className = "gray-text";
+
+        // Add link & name to the Item
+        week.appendChild(weekLink);
+        week.appendChild(weekName);
+
+        weeks.appendChild(week);
+    }
+}
+
+const weeksList = [ 'Scratch 😺', 'C','Arrays','Algorithms','Memory','Data Structures','Python','SQL','Information'];
+
+generateweeksList(weeksList);
